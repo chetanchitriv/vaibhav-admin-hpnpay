2cd855a4b8b8fd9d125be165bbf660375ed29736
vaibhav ghugase <vaibhavghugase00@gmail.com>
2023-11-22 16:08:49 +0530

    description

--- lib/constants/app_strings.dart
+++
@@ -1,9 +0,0 @@
-String dropdownValueCity = 'Select City';
-String dropdownValueState = 'Select State';
-
-String dropdownValueGender = 'Select';
-String dropdownValueLevel = 'Select';
-String dropdownValueLoanType = 'Select';
-String dropdownValuePaymentMethod = 'Select';
-String dropdownValueAdditional = 'Select';
-String dropdownValueBank = 'Select';
--- lib/repository/api.dart
+++
@@ -1,8 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = "https://avestan-be.onrender.com";
-  static const String verifyMobile = "/api/admin/verifyMobile";
-  static const String verifyOtp = "/api/admin/verifyOtp";
-  static const String createCompany = "/api/admin/createCompany";
-  static const String getAllCompanies = "/api/admin/getAllCompanies";
-  static const String createForm = "/api/admin/create/createForm";
-}
